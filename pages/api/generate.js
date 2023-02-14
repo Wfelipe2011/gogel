e9ac48019c382877292509103451f764884c010c
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2023-02-14 18:47:58 -0300

    ajuste na retentativa da pesquisa

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,17 +1,12 @@
 import { Configuration, OpenAIApi } from "openai";
 
-/**
- * Os preços são por 1.000 tokens.
- * Você pode pensar em tokens como pedaços de palavras, onde 1.000 tokens são cerca de 750 palavras.
- * Este parágrafo é de 35 tokens.
- */
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
+  let attempts = 0;
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -21,27 +16,40 @@ export default async function (req, res) {
     return;
   }
 
-  const input = req.body.input || '';
-   try {
-     const completion = await openai.createCompletion({
-       model: "text-davinci-003",
-       prompt: input,
-       max_tokens: 256,
-       temperature: 0.3,
-     });
-     res.status(200).json({ result: completion.data });
-   } catch (error) {
-     // Consider adjusting the error handling logic for your use case
-     if (error.response) {
-       console.error(error.response.status, error.response.data);
-       res.status(error.response.status).json(error.response.data);
-     } else {
-       console.error(`Error with OpenAI API request: ${error.message}`);
-       res.status(500).json({
-         error: {
-           message: "An error occurred during your request.",
-         },
-       });
-     }
-   }
+  let input = `${req.body.input || ''}
+  Responda em português de forma informal, de acordo com o contexto e de forma resumida.`;
+
+  async function createCompletion() {
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: input,
+        max_tokens: 700,
+        temperature: 0.3,
+      });
+      res.status(200).json({ result: completion.data });
+    } catch (error) {
+      if (attempts < 3) {
+        await sleep(1000);
+        await createCompletion();
+        return;
+      }
+
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "Tente novamente mais tarde.",
+        },
+      });
+    }
+  }
+  attempts++;
+  await createCompletion();
+}
+
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,19 @@ import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
+const COST_PER_TOKEN = 0.00004;
+const ERROR_MARGIN = 0.2;
+const DOLLAR_TO_BRL_RATE = 6;
+
+
 export default function Home() {
   const [textInput, setATextInput] = useState("");
+  const [rows, setRows] = useState(1);
   const [result, setResult] = useState();
+  const [cost, setCost] = useState({
+    tokens: 0,
+    price: 0,
+  });
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -22,14 +32,34 @@ export default function Home() {
         console.error(data.error);
         throw data.error || new Error(`Tente novamente`);
       }
-      // JSON.stringify(data, null, 2);
+      calculateCost(data.result.usage.total_tokens)
       setResult(data.result.choices[0].text);
-      setATextInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
+  }
+
+  function autoSizeTextarea(text) {
+    const charLength = text.length;
+    const divisor = window.innerWidth < 768 ? 30 : 60;
+    const rows = Math.floor(charLength / divisor) + 1;
+    setRows(rows);
+  }
+
+
+
+  function calculateCost(numTokens) {
+    let cost = numTokens * COST_PER_TOKEN;
+    cost = cost + (cost * ERROR_MARGIN);
+
+    const costInBRL = cost * DOLLAR_TO_BRL_RATE;
+
+    setCost({
+      tokens: numTokens,
+      price: costInBRL,
+    })
   }
 
   return (
@@ -46,14 +76,22 @@ export default function Home() {
           <textarea
             type="text"
             name="search"
-            rows={3}
+            rows={rows}
             placeholder="Faça uma pesquisa"
             value={textInput}
-            onChange={(e) => setATextInput(e.target.value)}
+            onChange={(e) => {
+              setATextInput(e.target.value);
+              autoSizeTextarea(e.target.value)
+            }}
           />
           <input type="submit" value="Perguntar" />
         </form>
         <div className={styles.result}>{result}</div>
+        <div className={styles.result}>
+          {cost.price && (
+            <p>Preço: R$ {cost.price.toFixed(2)}</p>
+          )}
+        </div>
       </main>
     </div>
   );
